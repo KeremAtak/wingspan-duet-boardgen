a0ad80e68f8a7ced8248faf1f4480622f515e9b0
Kerem Atak <kerem.atak@hotmail.com>
2024-05-04 18:02:54 +0300

    first commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to wingspan-duet-boardgen
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/wingspan_duet_boardgen/core_test.clj
@@ -0,0 +1,7 @@
+(ns wingspan-duet-boardgen.core-test
+  (:require [clojure.test :refer :all]
+            [wingspan-duet-boardgen.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
